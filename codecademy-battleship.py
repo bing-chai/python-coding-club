ea7d250dc6cf0ba6fdc6aef8521e2776658401d7
Bing <6682699+bing-chai@users.noreply.github.com>
2018-04-11 23:31:12 +1000

    Hide ship row and col

--- codecademy-battleship.py
+++ codecademy-battleship.py
@@ -19,8 +19,8 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-print ship_row
-print ship_col
+# print ship_row
+# print ship_col
 
 # Everything from here on should be in your for loop
 # don't forget to properly indent!
