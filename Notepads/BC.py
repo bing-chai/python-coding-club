b4e663e6cb9e3001a0aa13183030c887498d26c9
Bing <6682699+bing-chai@users.noreply.github.com>
2018-05-07 17:11:38 +1000

    badminton players class

--- Notepads/BC.py
+++ Notepads/BC.py
@@ -1,1 +1,19 @@
 # my first notepad commit!!!
+# my first class
+
+class BadmintonPlayer():
+  def __init__(self, name, age, gender):
+      self.name = name
+      self.age = age
+      self.gender = gender
+
+  def skills(forehand, backhand):
+      skills.forehand = 1
+      skills.backhand = 1
+
+  def movement(speed, agility):
+      movement.speed = 1
+      movement.agility = 1
+
+      
+
